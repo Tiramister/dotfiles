829926922b8a4d7108b568ffc514996f9bb584c1
tiramister <nh111424@gmail.com>
2023-02-07 21:54:17 +0900

    init.sh の改良

--- init.sh
+++ init.sh
@@ -1,35 +1,32 @@
-#!/bin/zsh
+#!/bin/sh
 
 # create symlinks from HOME to this repo
 src_dst_file_map=(
-  '.zshrc' '.zshrc'
-  'vim/.vimrc' '.vimrc'
-  'vim/config' '.vim/config'
-  'vim/coc-settings.json' '.vim/coc-settings.json'
+  '.zshrc:.zshrc'
+  'vim/.vimrc:.vimrc'
+  'vim/config:.vim/config'
+  'vim/coc-settings.json:.vim/coc-settings.json'
 )
 
-for i in $(seq 1 2 ${#src_dst_file_map[@]}); do
-  src="$(pwd)/${src_dst_file_map[i]}"
-  dst="${HOME}/${src_dst_file_map[i + 1]}"
+for line in ${src_dst_file_map[@]}; do
+  IFS=':' read -r src_file dst_file <<< "$line"
+  src="$(pwd)/${src_file}"
+  dst="${HOME}/${dst_file}"
 
   if [ ! -e "${src}" ]; then
     echo "[ERROR] No such file: ${src}" >&2
     exit 1
   fi
 
-  if [ -e "${dst}" ]; then
-    mv "${dst}" "${dst}.org"
-    echo "[INFO] ${dst} is renamed to ${dst}.org" >&2
+  if [ -e "$dst" ] && [ ! -L "$dst" ]; then
+    mv $dst "${dst}.org"
+    echo "[INFO] $dst is renamed to ${dst}.org" >&2
   fi
 
-  if [ -d "${dst}" ]; then
-    mkdir -p "${dst}"
-  else
-    mkdir -p "$(dirname ${dst})"
-  fi
+  mkdir -p "$(dirname $dst)"
 
-  ln -snf "${src}" "${dst}"
-  echo "[INFO] A symlink is created: ${dst} -> ${src}" >&2
+  ln -snf $src $dst
+  echo "[INFO] A symlink is created: $dst -> $src" >&2
 done
 
 # install vim plugins
