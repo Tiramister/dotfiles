29370ae0cda134cd3d434fc627dcd5ee7e4c44ba
Tiramister <contact@tiramister.net>
2024-05-12 19:58:27 +0900

    .gitignore をホワイトリスト化

--- .config/wezterm/.wezterm.lua
+++
@@ -1,5 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-
-return config
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,12 @@ local config = {}
 
 config.font_size = 16.0
 config.window_close_confirmation = 'NeverPrompt'
+config.font = wezterm.font_with_fallback {
+    'JetBrains Mono',
+    'Nerd Font Symbols',
+    'Noto Color Emoji',
+    'Noto Sans JP',
+}
 
 return config
 
